32126b761dd3bf446edea187f7b2544ecf63dadf
Mohammad Alam <mohammad.alam@blinemedical.com>
2016-07-03 12:52:27 -0400

    Add simple UDP Client socket implementation

---
+++ utilities/udpClientSocket.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+int32_t closeUdpClient(void *handle);
+int32_t isUdpClientRunning(void *handle);
+int32_t sendClientUdpData(void *handle, char *buffer, int32_t bufLen);
+void *initUdpClientSocket(uint16_t port, char *hostname);
+
