f5eafc266ac43290320c618ac0a7ff8814b159f3
Mohammad Alam <cheersalam@gmail.com>
2016-07-22 17:21:39 -0400

    Add utilities

---
+++ utilities/utilities.c
@@ -0,0 +1,49 @@
+#pragma once
+#include "stdint.h"
+
+// 1byte to uint32
+int32_t read1Bytetoint32(uint8_t *buffer, int32_t buffLen, int32_t *pos, uint32_t *outVal) {
+	if ( (NULL == buffer) || (*pos > buffLen)) {
+		return -1;
+	}
+	*outVal = (uint32_t)buffer[*pos++];
+	return 0;
+}
+
+// 1 byte to uint8_t
+int32_t read1Byteto1Byte(uint8_t *buffer, int32_t buffLen, int32_t *pos, uint8_t *outVal) {
+	if ((NULL == buffer) || (*pos > buffLen)) {
+		return -1;
+	}
+	*outVal = buffer[*pos++];
+	return 0;
+}
+
+// 4byte to uint32
+int32_t read4Bytetoint32(uint8_t *buffer, int32_t buffLen, int32_t *pos, uint32_t *outVal) {
+	if ((NULL == buffer) || ((*pos + 4) > buffLen)) {
+		return -1;
+	}
+	*outVal = ((buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | buffer[3]);
+	*pos += 4;
+	return 0;
+}
+
+
+
+int32_t readXBytestoint32(uint8_t *buffer, int32_t buffLen, int32_t xBytes, int32_t *pos, uint32_t *outVal) {
+	int32_t i = 0;
+	*outVal = 0;
+	if ((NULL == buffer) || (xBytes > 4) || (xBytes <= 0) || ((*pos + xBytes) > buffLen)) {
+		return -1;
+	}
+	for (i = 0; i < xBytes; i++) {
+		*outVal = (*outVal) | buffer[*pos++] << 8 * (xBytes - (i + 1));
+	}
+	return 0;
+}
+
+
+
+
+
---
+++ utilities/utilities.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "stdint.h"
+
+// 1byte to uint32
+int32_t read1Bytetoint32(uint8_t *buffer, int32_t buffLen, int32_t *pos, uint32_t *outVal);
+
+// 1 byte to uint8_t
+int32_t read1Byteto1Byte(uint8_t *buffer, int32_t buffLen, int32_t *pos, uint8_t *outVal);
+
+// 4byte to uint32
+int32_t read4Bytetoint32(uint8_t *buffer, int32_t buffLen, int32_t *pos, uint32_t *outVal);
+
+int32_t readXBytestoint32(uint8_t *buffer, int32_t buffLen, int32_t xBytes, int32_t *pos, uint32_t *outVal);
+
+
+
+
+
